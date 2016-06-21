e93f6c17f8e4bcc6b87e392852d423ded528e68f
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-06-21 20:27:42 -0300

    [ADD] generate user & groups list dinamically

--- src/views/groupsView.js
+++ src/views/groupsView.js
@@ -1,3 +1,23 @@
-groups.getGroups().then( (data) => {
-    console.log('groups', data);
+groups.getGroups().then( (groups) => {
+    console.log('groups', groups);
+
+    let groupListDiv = document.getElementById('group-list');
+
+    for (let i=0; i < groups.length; i++){
+        createGroupRow(groups[i]);
+    }
+
+    function createGroupRow(group) {
+        let rowDiv = document.createElement("div"),
+            name = document.createElement("div"),
+            id = document.createElement("div");
+
+        rowDiv.className = 'body_row';
+        name.textContent = group.name;
+        id.textContent = group.id;
+
+        rowDiv.appendChild(name);
+        rowDiv.appendChild(id);
+        groupListDiv.appendChild(rowDiv);
+    }
 });
--- src/views/userView.js
+++ src/views/userView.js
@@ -1,3 +1,23 @@
-users.getUsers().then( (data) => {
-    console.log('users', data);
+users.getUsers().then( (users) => {
+    console.log('users', users);
+
+    let userListDiv = document.getElementById('user-list');
+
+    for (let i=0; i < users.length; i++){
+        createUserRow(users[i]);
+    }
+
+    function createUserRow(user) {
+        let rowDiv = document.createElement("div"),
+            name = document.createElement("div"),
+            id = document.createElement("div");
+
+        rowDiv.className = 'body_row';
+        name.textContent = user.userName;
+        id.textContent = user.id;
+
+        rowDiv.appendChild(name);
+        rowDiv.appendChild(id);
+        userListDiv.appendChild(rowDiv);
+    }
 });
