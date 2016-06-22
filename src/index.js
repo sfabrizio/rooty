e0b7b595c4f9aeab3dee3793ae012bcbd8e66725
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-06-22 11:30:41 -0300

    [ADD] adapt new views and add event handling

--- src/index.js
+++ src/index.js
@@ -1,21 +1,63 @@
+//App Main controller:
+
+var views = {};
+
 const userIsLogged = () => {
-    //request to login endpoint
+    //TODO: request to login view here for see if the user is still logged
     return true;
 };
 
-const initApp = () => {
-    'use strict';
-};
-
-
-function documentReady() {
-    console.log('documentReady');
-    var event = new Event('loaded');
-    document.dispatchEvent(event);
-}
-
-if (userIsLogged()){
+if ( userIsLogged() ){
     initApp();
-} else {//redirecto to login page
-    window.location.href = "/login";
+} else {//redirec to login page
+    window.location.href = "/login"; //TODO: have a login page
 }
+
+function initApp() {
+    //connect models with the views:
+    userModel.getUsers().then( (users) => {
+        views.userView = userView;
+        views.userView.loadUsers(users);
+    });
+
+    groupModel.getGroups().then( (groups) => {
+        views.groupView = groupView;
+        views.groupView.loadGroups(groups);
+        loadDetailsEvent(); //TODO: wait to both promises get resolve and then call this method
+    });
+}
+
+
+//event handling:
+
+function showRelatedDetail(e){
+    let modelID = e.target.lastElementChild.innerText,
+        relatedView = e.srcElement.parentElement.id === 'group-list' ? views.groupView : views.userView;
+    relatedView.showDetail(modelID);
+}
+
+function loadDetailsEvent(){
+    function addEventListenerByClass(className, event, fn) {
+        const list = document.querySelectorAll(className);
+
+        for (let i = 0; i < list.length; i++) {
+            list[i].addEventListener(event, fn, false);
+        }
+    }
+    addEventListenerByClass('.body_row', 'click', showRelatedDetail);
+}
+
+//online-offline status
+window.addEventListener('load', function() {
+
+    function updateOnlineStatus() {
+        let status = document.getElementById("connection-status");
+        const condition = navigator.onLine ? "online" : "offline";
+        status.className = condition;
+        status.innerHTML = condition.toUpperCase();
+    }
+
+    window.addEventListener('online',  updateOnlineStatus);
+    window.addEventListener('offline', updateOnlineStatus);
+});
+
