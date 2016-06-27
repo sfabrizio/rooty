45c0af96b966bb955a3e5c855ad98452620401e3
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-06-27 02:43:24 -0300

    [ADD] Implement showUserDetail method

--- src/views/userView.js
+++ src/views/userView.js
@@ -32,18 +32,40 @@ var userView = (() => {
         id.textContent = user.id;
         rowDiv.appendChild(name);
         rowDiv.appendChild(id);
+        // rowDiv.setAttribute('id',`row-user-${user.id}`);
         userListDiv.appendChild(rowDiv);
     }
 
-    function showDetail(e) {
-        let modelID = e.target.lastElementChild.innerText,
-            detailUserxDiv = document.querySelector('#user-detail-container');
-        detailUserDiv.style.display = 'block';
+    function showDetail(userId) {
+        if (!userId) return;
+
+        let detailUserDiv = document.querySelector('#user-detail-container'),
+            user = userModel.getUser(userId);
+
+        setDetailUserDiv(user);
+
+        detailUserDiv.className = 'make-hidden';
+        setTimeout( ()=>{//animation
+            detailUserDiv.className = 'make-visible';
+        },300);
+    }
+
+    function setDetailUserDiv(user){
+        let firstName = document.querySelector('#user_firstName'),
+            lastName = document.querySelector('#user_lastName'),
+            isAdmin = document.querySelector('#user_isAdmin'),
+            groupId = document.querySelector('#user_group_id');
+
+        firstName.textContent = user.firstName;
+        lastName.textContent = user.lastName;
+        isAdmin.textContent = user.isAdmin;
+        groupId.textContent = user.group_id;
+
     }
 
     function hideDetail() {
-        let detailUserxDiv = document.querySelector('#user-detail-container');
-        detailUserDiv.style.display = 'none';
+        let detailUserDiv = document.querySelector('#user-detail-container');
+        detailUserDiv.className = 'make-hidden';
     }
 })();
 
