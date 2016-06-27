d9e1494e2b8cb6ca32714323b4883c326d692d39
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-06-27 13:55:01 -0300

    [MOD] encapsulate group model

--- src/models/group.js
+++ src/models/group.js
@@ -1,22 +1,27 @@
-var groupModel = {};
+var groupModel = ( () => {
+    'use strict';
+    var model = {};
 
-function getMocGroups() {
+    function getMocGroups() {
 
-    return new Promise(function (resolve, reject) {
-        let reqObj = new XMLHttpRequest();
-        reqObj.overrideMimeType("application/json");
-        reqObj.open('GET', 'data.json', true); //local file
-        reqObj.onreadystatechange = () => {
-            if (reqObj.readyState == 4 && reqObj.status == "200") {
-                //avoid async mode with the callback
-                let data = JSON.parse(reqObj.responseText);
-                resolve(data.groups);
-            } else if (reqObj.readyState == 4 && reqObj.status != "200") {
-                reject('loading error');
-            }
-        };
-        reqObj.send(null);
-    });
-}
+        return new Promise(function (resolve, reject) {
+            let reqObj = new XMLHttpRequest();
+            reqObj.overrideMimeType("application/json");
+            reqObj.open('GET', 'data.json', true); //local file
+            reqObj.onreadystatechange = () => {
+                if (reqObj.readyState == 4 && reqObj.status == "200") {
+                    const data = JSON.parse(reqObj.responseText);
+                    resolve(data.groups);
+                } else if (reqObj.readyState == 4 && reqObj.status != "200") {
+                    reject('loading error');
+                }
+            };
+            reqObj.send(null);
+        });
+    }
 
-groupModel.getGroups = getMocGroups;
+    model.getGroups = getMocGroups;
+
+    return model;
+
+})();
