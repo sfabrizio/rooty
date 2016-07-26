e2858be7644b862b48a8dd28132eedf0368bef95
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-07-26 17:55:01 -0300

    [ADD] set some values on config file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,8 @@
-export default {
-    backendUrl: 'http://localhost:3000'
-};
+const config = {};
+
+config.port = process.env.port || 9000;
+config.host = process.env.host || 'http://localhost';
+config.debug = process.env.debug || false;
+config.backendUrl = [config.host, '3000'].join(':'); //force mock api port for now.
+
+module.exports = config;
