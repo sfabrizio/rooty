85056aca538d16813c4a83d56b26a665bd65b969
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-07-26 18:14:33 -0300

    [FIX] port value enviroment on config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {};
 
-config.port = process.env.port || 9000;
+config.port = process.env.PORT || 9000;
 config.host = process.env.host || 'http://localhost';
 config.debug = process.env.debug || false;
 config.backendUrl = [config.host, '3000'].join(':'); //force mock api port for now.
